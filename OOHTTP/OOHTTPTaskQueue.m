19a0fed9c7cecbbbf337b511187f0e9c6a125a29
emsihyo <emsihyo@gmail.com>
2018-04-13 15:55:07 +0800

    no message

--- OOHTTP/OOHTTPTaskQueue.m
+++ OOHTTP/OOHTTPTaskQueue.m
@@ -281,13 +281,9 @@ NSErrorDomain const OOHTTPTaskErrorDomain = @"OOHTTPTaskErrorDomainKey";
     if (self.backgroundTaskId) return;
     self.backgroundTaskId=[[UIApplication sharedApplication]beginBackgroundTaskWithExpirationHandler:^{
         self.backgroundTaskId=UIBackgroundTaskInvalid;
-        if (!self.timer) return;
         dispatch_source_cancel(self.timer);
         self.timer=nil;
     }];
-    if (self.timer) {
-        dispatch_source_cancel(self.timer);
-    }
     __weak typeof(self)weakSelf=self;
     self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
     dispatch_source_set_timer(self.timer, DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
